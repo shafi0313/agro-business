555454fb3896d5a742ee72edf3fa23fb148a9405
shafi97 <msh.shafiul@gmail.com>
2022-04-16 11:09:51 +0600

    coll

--- resources/views/admin/account/received/index.blade.php
+++ resources/views/admin/account/received/index.blade.php
@@ -74,56 +74,7 @@
 </div>
 
 @push('custom_scripts')
-<script >
-    $(document).ready(function() {
-        $('#basic-datatables').DataTable({
-
-        });
-
-        $('#multi-filter-select').DataTable( {
-            "lengthMenu": [[50, 100, 200, -1], [50, 100, 200, "All"]],
-            // "pageLength": 10,
-            initComplete: function () {
-                this.api().columns().every( function () {
-                    var column = this;
-                    var select = $('<select class="form-control form-control-sm"><option value=""></option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                            );
-
-                        column
-                        .search( val ? '^'+val+'$' : '', true, false )
-                        .draw();
-                    } );
-
-                    column.data().unique().sort().each( function ( d, j ) {
-                        select.append( '<option value="'+d+'">'+d+'</option>' )
-                    } );
-                } );
-            }
-        });
-
-        // Add Row
-        $('#add-row').DataTable({
-            "pageLength": 5,
-        });
-
-        var action = '<td> <div class="form-button-action"> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
-
-        // $('#addRowButton').click(function() {
-        //     $('#add-row').dataTable().fnAddData([
-        //         $("#addName").val(),
-        //         $("#addPosition").val(),
-        //         $("#addOffice").val(),
-        //         action
-        //         ]);
-        //     $('#addRowModal').modal('hide');
-
-        // });
-    });
-</script>
+@include('admin.include.data_table_js')
 
 @endpush
 @endsection
