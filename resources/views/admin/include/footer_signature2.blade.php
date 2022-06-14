9c45073e837349504398ef66995dc90009183a40
shafi97 <msh.shafiul@gmail.com>
2022-06-14 16:28:42 +0600

    signeture2 footer

--- resources/views/admin/include/footer_signature2.blade.php
+++ resources/views/admin/include/footer_signature2.blade.php
@@ -1,16 +1,43 @@
+
 <style>
-    .footer_signature {
+    /* .footer_signature {
         padding: 15px 15px 0 15px;
-    }
+    } */
     .footer_signature div {
-        border-top: 1px solid black;
+        border-bottom: 1px solid black;
+    }
+
+    /* .footer_signature_area{
+        position: fixed;
+        bottom: 0;
+    } */
+
+    @page {
+        size: A4;
+        margin: 11mm 11mm 18mm 11mm;
+    }
+
+    @media print {
+        .footer_signature .p {
+            position: fixed;
+            bottom: 0;
+            width: 200px;
+            left: 0;
+            text-align: center;
+        }
+
+        .footer_signature .a {
+            position: fixed;
+            bottom: 0;
+            width: 200px;
+            right: 0;
+            text-align: center;
+        }
     }
 </style>
 <div id="footer_signature_area" style="display: none">
-    <div class="footer_signature" style="display:flex; justify-content: space-between;">
-        <div>Prepared by</div>
-        {{-- <div>Checked by</div> --}}
-        {{-- <div style="padding:0 40px">CEO</div> --}}
-        <div>Authorized by</div>
+    <div class="footer_signature">
+        <div class="p">Prepared by</div>
+        <div  class="a">Authorized by</div>
     </div>
 </div>
