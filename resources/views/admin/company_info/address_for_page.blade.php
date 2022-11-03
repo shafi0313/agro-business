1bdd8e23da7f907eb2c360e527b4938fcaae3d73
shafi97 <msh.shafiul@gmail.com>
2022-11-03 10:05:48 +0600

    c

--- resources/views/admin/company_info/address_for_page.blade.php
+++ resources/views/admin/company_info/address_for_page.blade.php
@@ -3,7 +3,7 @@
 @endphp
 <p style="font-size: 22px;margin:0">{{ config('app.locale')=='en'?setting('app_name'):setting('app_name_b') }}</p>
 <p>
-    <span>{{ $companyInfo->address }}</span><br>
-    <span>Phone: {{ $companyInfo->phone }}</span><br>
-    <span>Email: {{ $companyInfo->web }}</span>
+    <span>{{ setting('front_address') }}</span><br>
+    <span>Phone: {{ setting('phone1') }}</span><br>
+    <span>Email: {{ setting('app_url') }}</span>
 </p>
